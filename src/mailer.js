784882c575faeba88e64dfeabc0456a87c5132bc
carlos padial <surreal@asturnazari.com>
2021-06-25 12:25:13 +0200

    daily and weekly reports, mailer and cron logic

--- src/mailer.js
+++ src/mailer.js
@@ -1,9 +1,7 @@
 const nodemailer = require("nodemailer");
-const cron = require('node-cron');
-const fs = require('./traffic-log-management');
 
 module.exports = {
-    sendLogMail: function (host, user, pass, to, bcc, fileName, fileUrl) {
+    sendLogMail: function (host, user, pass, to, bcc, subject, content, fileName, fileUrl, callback) {
         async function sendMail() {
             let transporter = nodemailer.createTransport({
                 host: host,
@@ -14,52 +12,44 @@ module.exports = {
                     pass: pass,
                 },
             });
-            let time = new Date();
-            let message = await transporter.sendMail({
+            let msg = {
                 from: user,
                 to: to,
                 bcc: bcc,
-                subject: "📊📋 Log likelike Test ✔ 🕰️📬 " + time.toLocaleString(), // Subject line
-                text: "Hello world",
-                html: "<b>Hello world?</b>",
-                attachments: {
+                subject: subject,
+                text: content,
+            };
+
+            if (fileName != null) {
+                msg.attachments = {
                     filename: fileName,
                     path: fileUrl
                 }
-            });
+            }
 
-            console.log("----- Message sent to: %s", to);
+            let message = await transporter.sendMail(msg);
         }
 
         sendMail().then(function() {
-            console.log('--- backup and clear logs???????');
+            console.log("----- Message sent to: %s", to, bcc);
+            if (callback) {
+                callback();
+            }
         }).catch(console.error);
     },
-    setupMail: function() {
-        const fileName = 'players-log.txt';
-        const fileUrl = './logs/players-log.txt';
-        const weeklyUrl = './logs/weeklyFile.csv';
-
+    sendMail: function(subject, content, fileName, fileUrl, callback) {
         this.sendLogMail(
             process.env.MAILHOST, 
             process.env.MAILUSER, 
             process.env.MAILPASS, 
             process.env.MAILTO, 
             process.env.MAILBCC,
+            subject,
+            content,
             fileName,
-            fileUrl
+            fileUrl,
+            callback
         );
-
-        cron.schedule('59 23 * * *', () => {
-            console.log('running a task every day at 23:59');
-            fs.appendToFile(weeklyUrl, 'running a task every day at 23:59\n')
-        });
-
-        cron.schedule('* * * * *', () => {
-            console.log('running a task every minute');
-            //sendMail(fileName, fileUrl);
-            fs.appendToFile(weeklyUrl, fs.timeToFileName(new Date()) + ' \n')
-        });
     }
 };
 
--- src/traffic-log-management.js
+++ src/traffic-log-management.js
@@ -1,10 +1,18 @@
 const fs = require('fs');
-
-const currentLog = './logs/players-log.txt';
+const path = require('path');
+const { start } = require('repl');
+const mailer = require('./mailer');
 
 module.exports = {
     timeToFileName: function (time) {
         return time.toISOString().split('.')[0];
+    },
+    timeToLocale: function(time) {
+        if (!time) {
+            time = new Date(Date.now());
+        }
+        let localeTime = new Date(time.getTime() - (time.getTimezoneOffset() * 60000));
+        return localeTime;
     },
     writeToFile: function (file, data) {
         fs.writeFile(file, data, 'utf8', (err) => {
@@ -12,26 +20,142 @@ module.exports = {
             console.log(`File ` + file + ` is written successfully!`);
         });
     },
-    appendToFile: function (file, data) {
-        fs.appendFile(file, data, (err) => {
+    // data is an array with all data
+    appendToLog: function (file, id, action, data) {
+        let time = this.timeToLocale();
+        let timeString = time.toISOString();
+        let content = [timeString, id, action].join(',');
+        if (data) {
+            content = [content, data.join(',')].join(',');
+        }
+        content += '\n';
+        fs.appendFile(file, content, (err) => {
             if (err) return console.log(`Error appending to file ` + file + ` : ${err}`);
             console.log(`File ` + file + ` is updated successfully!`);
         })
     },
-    appendToLog: function (data) {
-        this.appendToFile(currentLog, data);
+    serverStart: function (startTime) {
+        startTime = this.timeToLocale(new Date(startTime))
+        startTime = this.timeToFileName(new Date(startTime));
+        let logFileName = './logs/' + startTime + '.txt';
+        this.writeToFile(logFileName, startTime + ',serverStart\n');
+        return logFileName;
     },
-    serverStart: function (startTime, url) {
-        let timeString = this.timeToFileName(new Date());
-        fs.rename(currentLog, './logs/trafficLog_' + timeString + '.csv', function() {
-            console.log('renombrados ficheros último reset');
-        });
-        this.writeToFile(currentLog, 'server started ' + startTime + '\n');
+    changeLogFileName: function (time) {
+        time = this.timeToLocale(new Date(time));
+        time = this.timeToFileName(new Date(time));
+        let logFileName = './logs/' + time + '.txt';
+        // this.writeToFile(logFileName, time + ',new log file\n');
+        return logFileName;
     },
     generateDailyLog: function () {
-        let timeString = this.timeToFileName(new Date());
-        fs.rename(currentLog, './logs/log_' + timeString + '.csv', function() {
-            console.log('generate log ' + timeString);
+        let date = new Date();
+        var yesterday = new Date(date.getTime());
+        yesterday.setDate(date.getDate() - 1);
+
+        let yesterdayString = yesterday.toISOString().split('T')[0];
+        let logDir = path.join(__dirname, '../logs');
+        let archiveDir = path.join(__dirname, '../logs/archive');
+
+        // fake date for testing purposes
+        // yesterdayString = '2021-06-24';
+        let dailyLog = path.join(logDir, yesterdayString + '.txt');
+
+        let appendFile = fs.appendFile;
+        fs.readdir(logDir, function (err, files) {
+            let fileCount = 0;
+            for (let index = 0; index < files.length; index++) {
+                const file = files[index];
+                if (file !== yesterdayString && file.split('T')[0] === yesterdayString) {
+                    fileCount++;
+                    let filePath = path.join(logDir, file);
+                    fs.readFile(filePath, function(err, buf) {
+                        appendFile(dailyLog, buf.toString(), function() {
+                            fs.rename(filePath, path.join(archiveDir, file), function() {
+                                console.log(`File ` + yesterdayString + `.txt is updated successfully!`);
+                                console.log('moving log ' + file + ' to archive folder');
+                            });
+                        });
+                    });
+                }
+            }
+            if (fileCount > 0) {
+                console.log('--> daily log ' + yesterdayString + '.txt generated');
+            } else {
+                console.log('--> skip daily generation');
+            }
         });
+
+        return yesterdayString;
     },
+    getPreviousWeekArray: function () {
+        let currentDate = new Date();
+        var yesterday = new Date(currentDate.getTime());
+        yesterday.setDate(currentDate.getDate() - 1);
+
+        var weekDates = [];
+        for (let index = 7; index > 0; index--) {
+            let date = new Date(currentDate.getTime());
+            date.setDate(currentDate.getDate() - index)
+            weekDates.push(date.toISOString().split('T')[0]);
+        }
+
+        return weekDates;
+    },
+    getPreviousWeekFileName: function (weekDates) {
+        return weekDates[0] + '_to_' + weekDates[6];
+    },
+    generateWeeklyLog: function () {
+        // this runs every monday and look for dailys from previous monday (7 days ago) 
+        // to previous sunday (yesterday)
+        let weekDates = this.getPreviousWeekArray();
+        let weeklyString = this.getPreviousWeekFileName(weekDates);
+        console.log('generating weekly log' + weeklyString);
+        console.log('previous week', weekDates);
+        console.log('week file ', weeklyString);
+
+        let logDir = path.join(__dirname, '../logs');
+        let archiveDir = path.join(__dirname, '../logs/archiveDaily');
+        let weeklyLog = path.join(logDir, weeklyString + '.txt');
+
+        let appendFile = fs.appendFile;
+        fs.readdir(logDir, function(err, files) {
+            let fileCount = 0;
+            for (let index = 0; index < files.length; index++) {
+                const file = files[index];
+                if (weekDates.includes(file.split('.')[0])) {
+                    console.log('incluido ' + file);
+                    fileCount++;
+                    let filePath = path.join(logDir, file);
+                    fs.readFile(filePath, function(err, buf) {
+                        appendFile(weeklyLog, buf.toString(), function() {
+                            fs.rename(filePath, path.join(archiveDir, file), function() {
+                                console.log(`File ` + weeklyString + `.txt is updated successfully!`);
+                                console.log('moving daily ' + file + ' to archive folder');
+                            });
+                        });
+                    })
+                }
+            }
+        });
+
+        return weeklyString;
+    },
+    sendWeeklyLog: function () {
+        let weekDates = this.getPreviousWeekArray();
+        let weeklyString = this.getPreviousWeekFileName(weekDates);
+        let logDir = path.join(__dirname, '../logs');
+        let archiveDir = path.join(__dirname, '../logs/archiveWeekly');
+        let weeklyLog = path.join(logDir, weeklyString + '.txt');
+
+        console.log('sending file ' + weeklyString);
+        
+        let subject = "📊📋 Log likelike Test ✔ 🕰️📬 " + weeklyString;
+        let content = "Automatic traffic report. Do not reply.";
+        mailer.sendMail(subject, content, weeklyString + '.txt', weeklyLog, function() {
+            fs.rename(weeklyLog, path.join(archiveDir, weeklyString + '.txt'), function() {
+                console.log('moving weekly ' + weeklyString + ' to archive folder');
+            })
+        });
+    }
 };
