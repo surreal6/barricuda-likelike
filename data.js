3b4c4378fd1ed42919174c6f243d0a2d05eb2fb6
carlos padial <surreal@asturnazari.com>
2021-07-06 14:53:54 +0200

    add some rooms

--- data.js
+++ data.js
@@ -2,7 +2,7 @@
 //they are either related to the rooms or shared with the server 
 module.exports.SETTINGS = {
     //if not specified by the url where is the starting point
-    defaultRoom: "hall",
+    defaultRoom: "r01Patio",
     //minimum time between talk messages enforced by both client and server
     ANTI_SPAM: 1000,
     //shows up at first non lurking login
@@ -23,135 +23,244 @@ module.exports.SOUNDS = [
 
 module.exports.ROOMS = {
 
-    hall: {
-        bg: "/barricuda/entrada-spritesheet.png",
-        frames: 4,
-        animations: { 
-            bg0000: [1, 4], 
-            bg0001: [5, 8], 
-            bg0010: [9, 12], 
-            bg0011: [13, 16], 
-            bg0100: [17, 20], 
-            bg0101: [21, 24], 
-            bg0110: [25, 28], 
-            bg0111: [29, 32], 
-            bg1000: [33, 36], 
-            bg1001: [37, 40], 
-            bg1010: [41, 44], 
-            bg1011: [45, 48], 
-            bg1100: [49, 52], 
-            bg1101: [53, 56], 
-            bg1110: [57, 60], 
-            bg1111: [61, 64]
-        },
+    r01Patio: {
+        bg: "/salas/r01Patio.png",
         frameDelay: 60,
         avatarScale: 3,
         pageBg: "#1c2016",
-        area: "/barricuda/entrada-areas.png",
+        area: "/salas/r01Patio_Int.png",
         tint: "#fdeac8",
         bubblesY: 44,
-        spawn: [80, 80, 120, 98],
+        spawn: [8, 75, 120, 90],
         areaColors: {
             //h will be replaced by #
-            // #ff77a8
-            hff77a8: { cmd: "enter", room: "classroom", label: "classroom", point: [10, 84], enterPoint: [118, 87], obstacle: false },
-            // #ce9600
-            hce9600: { cmd: "enter", room: "cave", label: "cave", point: [34, 70], enterPoint: [30, 19], obstacle: false },
-            // #15ce00
-            h15ce00: { cmd: "enter", room: "calle", label: "Calle Ave María", point: [112, 71], enterPoint: [64, 50], obstacle: false },
-            // #00ffff
-            h00ffff: { cmd: "action", label: "classroom switch", point: [43, 70], actionId: 'ClassroomLight', obstacle: false },
-            // #ff00ff
-            hff00ff: { cmd: "action", label: "cave switch", point: [47, 70], actionId: 'CaveLight', obstacle: false },
-            // #ffff00
-            hffff00: { cmd: "action", label: "hall switch", point: [51, 70], actionId: 'HallLight', obstacle: false },
-            // #0066ff
-            h0066ff: { cmd: "text", label: "La Barricuda", point: [88, 63], iframe: false, txt: 'La Barricuda fue la escuela de\ncreación audiovisual del colectivo\nAventuras Visuales de 2006 a 2009', lines: 3, url: "http://labarricuda.blogspot.com/2006/10/instalaciones.html", obstacle: false }
-        },
-        things: {
-            columna: { file: "/barricuda/barri_entrada-columna.png", id: "columna", position: [53, 35], frames: 1, frameDelay: 60, visible: true },
-            mesa: { file: "/barricuda/barri_entrada-mesa.png", id: "mesa", offset: 10, position: [59, 46], frames: 1, frameDelay: 60, visible: true, label: "Cursos y talleres", command: { cmd: "text", iframe: false, txt: "En este local se realizaron cursos y talleres de practicamente todas las disciplinas cinematográficas", align: "left", lines: 3, url: "http://labarricuda.blogspot.com/2006/10/presentacin.html", point: [82, 75] } },
-            
+            // // #0066ff
+            h0066ff: { cmd: "text", label: "créditos", point: [50, 80], obstacle: false },
+            // #ff9900
+            hff9900: { cmd: "enter", room: "r02Entrada", label: "entrada", point: [64, 60], enterPoint: [65, 95], obstacle: false },
         }
     },
-    classroom: {
-        bg: "/barricuda/aula-spritesheet.png",
-        frames: 4,
-        animations: { 
-            bg0000: [1, 4], 
-            bg0001: [5, 8], 
-            bg0010: [9, 12], 
-            bg0011: [13, 16], 
-            bg0100: [17, 20], 
-            bg0101: [21, 24], 
-            bg0110: [25, 28], 
-            bg0111: [29, 32], 
-            bg1000: [33, 36], 
-            bg1001: [37, 40], 
-            bg1010: [41, 44], 
-            bg1011: [45, 48], 
-            bg1100: [49, 52], 
-            bg1101: [53, 56], 
-            bg1110: [57, 60], 
-            bg1111: [61, 64]
-        },
+    r02Entrada: {
+        bg: "/salas/r02Entrada.png",
         frameDelay: 60,
         avatarScale: 3,
-        pageBg: "#765f52",
-        area: "/barricuda/aula-areas.png",
+        pageBg: "#1c2016",
+        area: "/salas/r02Entrada_Int.png",
         tint: "#fdeac8",
         bubblesY: 44,
-        spawn: [14, 84, 119, 92],
+        spawn: [60, 88, 70, 95],
         areaColors: {
             //h will be replaced by #
-            // #ff77a8
-            hff77a8: { cmd: "enter", room: "hall", label: "hall", point: [118, 87], enterPoint: [20, 84], obstacle: false },
+            // #ff0000
+            hff0000: { cmd: "enter", room: "r01Patio", label: "patio", point: [66, 98], enterPoint: [64, 65], obstacle: false },
             // #0066ff
-            h0066ff: { cmd: "text", iframe: true, label: "Cortometrajes", point: [62, 67], txt: 'Entre 2003 y 2009 los aventureros visuales realizaron casi un centenar de cortometrajes y piezas audiovisuales', lines: 4, url: "https://www.youtube.com/embed/videoseries?list=PLFF29FEE8F9D192C9", obstacle: false },
-            // #00ff00
-            h00ff00: { cmd: "action", label: "Medvedkin", actionId: "Medvedkin", point: [84, 69], obstacle: false },
-        },
-        things: {
-            silla1: { file: "/barricuda/sillas2.png", id: "silla1", scale: 1.4, position: [10, 55], frames: 1, frameDelay: 60, visible: true },
-            silla2: { file: "/barricuda/sillas.png", id: "silla2", scale: 1.6, position: [10, 65], frames: 1, frameDelay: 60, visible: true },
-            silla3: { file: "/barricuda/sillas.png", id: "silla3", scale: 2, position: [10, 80], frames: 1, frameDelay: 60, visible: true },
+            h0066ff: { cmd: "enter", room: "r04SalaX", label: "?", point: [98, 72], enterPoint: [60, 95], obstacle: false },
+            // #ff9900
+            hff9900: { cmd: "enter", room: "r03Cookies", label: "?", point: [32, 72], enterPoint: [70, 95], obstacle: false },
+            // #999900
+            h999900: { cmd: "text", label: "?", point: [65, 95], obstacle: false },
+            // #ff00ff
+            hff00ff: { cmd: "text", label: "?", point: [65, 95], obstacle: false },
         }
     },
-    cave: {
-        bg: "/barricuda/cueva-spritesheet.png",
-        frames: 4,
-        animations: { 
-            bg0000: [1, 4], 
-            bg0001: [5, 8], 
-            bg0010: [9, 12], 
-            bg0011: [13, 16], 
-            bg0100: [17, 20], 
-            bg0101: [21, 24], 
-            bg0110: [25, 28], 
-            bg0111: [29, 32], 
-            bg1000: [1, 4],
-            bg1001: [5, 8],
-            bg1010: [9, 12],
-            bg1011: [13, 16],
-            bg1100: [17, 20],
-            bg1101: [21, 24],
-            bg1110: [25, 28],
-            bg1111: [29, 32]},
-        perspective: "cuevaPerspective",
+    r03Cookies: {
+        bg: "/salas/r03Cookies.png",
         frameDelay: 60,
-        avatarScale: 5,
-        pageBg: "#765f52",
-        area: "/barricuda/cueva-areas.png",
+        avatarScale: 3,
+        pageBg: "#1c2016",
+        area: "/salas/r03Cookies_Int.png",
         tint: "#fdeac8",
-        bubblesY: 14,
-        spawn: [68, 60, 82, 70],
+        bubblesY: 44,
+        spawn: [60, 88, 70, 95],
         areaColors: {
             //h will be replaced by #
-            // #ce7b00
-            hce7b00: { cmd: "enter", room: "hall", label: "hall", point: [30, 19], enterPoint: [34, 70], obstacle: false }
-        },
-        things: {
-            techo: { file: "/barricuda/techo-sotano.png", id: "techo", position: [0, 0], offset: 100, frames: 1, frameDelay: 60, visible: true },
+            // #ff0000
+            hff0000: { cmd: "enter", room: "r02Entrada", label: "entrada", point: [66, 98], enterPoint: [32, 72], obstacle: false },
+            // #0066ff
+            h0066ff: { cmd: "text", label: "galletas", point: [45, 82], obstacle: false },
+            // #29adff
+            h29adff: { cmd: "enter", room: "r05SalaXCopia", label: "?", point: [65, 72], enterPoint: [60, 95], obstacle: false },
+            // #ffec27
+            hffec27: { cmd: "enter", room: "r08Huella", label: "huella", point: [10, 85], enterPoint: [120, 84], obstacle: false },
+            // #00cc99
+            h00cc99: { cmd: "text", label: "robot", point: [37, 85], obstacle: false },
+            //
+            // CONVERTIR EN objeto
+            // #ff00ff
+            hff00ff: { cmd: "text", label: "mostruo de las galletas", point: [87, 89], obstacle: false },
         }
-    }
+    },
+    r04SalaX: {
+        bg: "/salas/r04SalaX.png",
+        frameDelay: 60,
+        avatarScale: 3,
+        pageBg: "#1c2016",
+        area: "/salas/r04SalaX_Int.png",
+        tint: "#fdeac8",
+        bubblesY: 44,
+        spawn: [60, 88, 70, 95],
+        areaColors: {
+            //h will be replaced by #
+            // #ff0000
+            hff0000: { cmd: "enter", room: "r02Entrada", label: "entrada", point: [60, 95], enterPoint: [98, 72], obstacle: false },
+            // #0066ff
+            h0066ff: { cmd: "text", label: "?", point: [80, 78], obstacle: false },
+            // #566db8
+            h566db8: { cmd: "text", label: "?", point: [45, 82], obstacle: false },
+        }
+    },
+    r05SalaXCopia: {
+        bg: "/salas/r05SalaXCopia.png",
+        frameDelay: 60,
+        avatarScale: 3,
+        pageBg: "#1c2016",
+        area: "/salas/r05SalaXCopia_Int.png",
+        tint: "#fdeac8",
+        bubblesY: 44,
+        spawn: [60, 88, 70, 95],
+        areaColors: {
+            //h will be replaced by #
+            // #ff0000
+            hff0000: { cmd: "enter", room: "r03Cookies", label: "cookies", point: [60, 95], enterPoint: [65, 72], obstacle: false },
+            // #0066ff
+            h0066ff: { cmd: "text", label: "?", point: [80, 78], obstacle: false },
+            // #566db8
+            h566db8: { cmd: "text", label: "?", point: [45, 82], obstacle: false },
+        }
+    },
+    r08Huella: {
+        bg: "/salas/r08Huella.png",
+        frameDelay: 60,
+        avatarScale: 3,
+        pageBg: "#1c2016",
+        area: "/salas/r08Huella_Int.png",
+        tint: "#fdeac8",
+        bubblesY: 44,
+        spawn: [60, 88, 70, 95],
+        areaColors: {
+            //h will be replaced by #
+            // #ff0000
+            hff0000: { cmd: "enter", room: "r06Reciclaje", label: "reciclaje", point: [92, 95], enterPoint: [68, 47], obstacle: false },
+            // #00e436
+            h00e436: { cmd: "enter", room: "r03Cookies", label: "cookies", point: [120, 84], enterPoint: [10, 85], obstacle: false },
+            // #ff77a8
+            hff77a8: { cmd: "enter", room: "r10Nubes", label: "nubes", point: [98, 72], enterPoint: [60, 98], obstacle: false },
+            // #29adff
+            h29adff: { cmd: "enter", room: "r10Nubes", label: "nubes", point: [32, 72], enterPoint: [60, 98], obstacle: false },
+            // #ffec27
+            hffec27: { cmd: "enter", room: "r09FakeNews", label: "fake", point: [10, 85], enterPoint: [120, 86], obstacle: false },
+            // #be1250
+            hbe1250: { cmd: "enter", room: "r07Marionetas", label: "marionetas", point: [36, 95], enterPoint: [60, 95], obstacle: false },
+            //
+            // CONVERTIR EN objeto
+            // #9900cc
+            h9900cc: { cmd: "text", label: "personaje", point: [69, 88], obstacle: false },
+            // #ff9900
+            hff9900: { cmd: "text", label: "espejo", point: [36, 84], obstacle: false },
+            // #0066ff
+            h0066ff: { cmd: "text", label: "espejo", point: [36, 84], obstacle: false },
+            // #ff00ff
+            hff00ff: { cmd: "text", label: "espejo", point: [36, 84], obstacle: false },
+            // #0f8a61
+            h0f8a61: { cmd: "text", label: "espejo", point: [87, 84], obstacle: false },
+            // #999900
+            h999900: { cmd: "text", label: "espejo", point: [87, 84], obstacle: false },
+            // #00cc99
+            h00cc99: { cmd: "text", label: "espejo", point: [87, 84], obstacle: false },
+        }
+    },
+    r06Reciclaje: {
+        bg: "/salas/r06Reciclaje.png",
+        frameDelay: 60,
+        avatarScale: 3,
+        pageBg: "#1c2016",
+        area: "/salas/r06Reciclaje_Int.png",
+        tint: "#fdeac8",
+        bubblesY: 44,
+        spawn: [58, 69, 75, 75],
+        areaColors: {
+            //h will be replaced by #
+            // #29adff
+            h29adff: { cmd: "enter", room: "r08Huella", label: "huella", point: [68, 47], enterPoint: [92, 95], obstacle: false },
+            // #ffec27
+            hffec27: { cmd: "enter", room: "r07Marionetas", label: "marionetas", point: [10, 85], enterPoint: [65, 72], obstacle: false },
+            // #0066ff
+            h0066ff: { cmd: "text", label: "residuo", point: [86, 87], obstacle: false },
+            // #00cc99
+            h00cc99: { cmd: "text", label: "papelera", point: [60, 80], obstacle: false },
+            // #ff00ff
+            hff00ff: { cmd: "text", label: "papelera", point: [60, 80], obstacle: false },
+            // #ff9900
+            hff9900: { cmd: "text", label: "residuo", point: [40, 80], obstacle: false },
+        }
+    },
+    r09FakeNews: {
+        bg: "/salas/r09FakeNews.png",
+        frameDelay: 60,
+        avatarScale: 3,
+        pageBg: "#1c2016",
+        area: "/salas/r09FakeNews_Int.png",
+        tint: "#fdeac8",
+        bubblesY: 44,
+        spawn: [40, 85, 90, 95],
+        areaColors: {
+            //h will be replaced by #
+            // #00e436
+            h00e436: { cmd: "enter", room: "r08Huella", label: "huella", point: [120, 85], enterPoint: [10, 85], obstacle: false },
+            // #ffec27
+            hffec27: { cmd: "enter", room: "r10Nubes", label: "nubes", point: [10, 85], enterPoint: [60, 98], obstacle: false },
+            // #00cc99
+            h00cc99: { cmd: "text", label: "news", point: [97, 80], obstacle: false },
+            // #ff00ff
+            hff00ff: { cmd: "text", label: "news", point: [30, 80], obstacle: false },
+            // #9900cc
+            h9900cc: { cmd: "text", label: "personaje", point: [69, 80], obstacle: false },
+        }
+    },
+    r10Nubes: {
+        bg: "/salas/r10Nubes.png",
+        frameDelay: 60,
+        avatarScale: 3,
+        pageBg: "#1c2016",
+        area: "/salas/r10Nubes_Int.png",
+        tint: "#fdeac8",
+        bubblesY: 44,
+        spawn: [40, 85, 90, 95],
+        areaColors: {
+            //h will be replaced by #
+
+            // HABRIA QUE PONER OTRA SALIDA???
+            // #ff0000
+            hff0000: { cmd: "enter", room: "r08Huella", label: "huella", point: [60, 98], enterPoint: [32, 72], obstacle: false },
+            // #ff77a8
+            hff77a8: { cmd: "enter", room: "r13Netiqueta", label: "planta -1", point: [86, 77], enterPoint: [60, 95], obstacle: false },
+            // #b75714
+            hb75714: { cmd: "text", label: "cartel", point: [60, 80], obstacle: false },
+            // #ff00ff
+            hff00ff: { cmd: "text", label: "nube", point: [60, 80], obstacle: false },
+            // #ff9900
+            hff9900: { cmd: "text", label: "nube", point: [60, 80], obstacle: false },
+            // #689156
+            h689156: { cmd: "text", label: "nube", point: [60, 80], obstacle: false },
+            // #9900cc
+            h9900cc: { cmd: "text", label: "nube", point: [60, 80], obstacle: false },
+            // #0066ff
+            h0066ff: { cmd: "text", label: "nube", point: [60, 80], obstacle: false },
+        }
+    },
+    r13Netiqueta: {
+        bg: "/salas/r13Netiqueta.png",
+        frameDelay: 60,
+        avatarScale: 3,
+        pageBg: "#1c2016",
+        area: "/salas/r13Netiqueta_Int.png",
+        tint: "#fdeac8",
+        bubblesY: 44,
+        spawn: [40, 85, 90, 95],
+        areaColors: {
+            //h will be replaced by #
+            // #be1250
+            hbe1250: { cmd: "enter", room: "r10Nubes", label: "nubes", point: [60, 95], enterPoint: [86, 77], obstacle: false },
+        }
+    },
 };
