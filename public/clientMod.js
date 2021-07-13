f794def3b7600567d647822845642559a4f51b00
carlos padial <surreal@asturnazari.com>
2021-07-13 13:39:45 +0200

    add survey in section - feature

--- public/clientMod.js
+++ public/clientMod.js
@@ -36,3 +36,141 @@ function cuevaPerspective(sprite) {
     }
     return s;
 }
+
+function afterPool(data) {
+    console.log("Answers ", data);
+}
+
+const POOLS = {
+    pool1: {
+        actionId: "sendPoolfromSection",
+        postAction: "afterPool",
+        questions: {
+            question1: {
+                type: 'input',
+                text: 'question1',
+                label: 'label question1'
+            },
+            question2: {
+                type: 'checkbox',
+                text: 'select some of this',
+                options: [
+                    {
+                        text: "option1",
+                        label: 'label option1',
+                        class: 'aaa'
+                    },
+                    {
+                        text: "option2",
+                        label: 'label option2',
+                        class: 'bbb'
+                    },
+                    {
+                        text: "option3",
+                        label: 'label option3',
+                        class: 'ccc'
+                    },
+                ]
+                
+            },
+            question3: {
+                type: 'radio',
+                text: 'select one of this',
+                options: [
+                    {
+                        text: "option1",
+                        label: 'label option1'
+                    },
+                    {
+                        text: "option2",
+                        label: 'label option2'
+                    },
+                    {
+                        text: "option3",
+                        label: 'label option3'
+                    },
+                ]
+                
+            },
+        }
+    },
+    pool2: {
+        actionId: "sendPoolfromSection",
+        postAction: "afterPool",
+        questions: {
+            question1: {
+                type: 'input',
+                inputType: 'password',
+                text: 'text question1',
+                label: 'label question1'
+            },
+            question2: {
+                type: 'radio',
+                text: 'select one of this options',
+                options: [
+                    {
+                        text: "option 1",
+                        label: 'label option1',
+                        class: 'aaa'
+                    },
+                    {
+                        text: "option 2",
+                        label: 'label option2',
+                        class: 'bbb'
+                    },
+                    {
+                        text: "option 3",
+                        label: 'label option3',
+                        class: 'ccc'
+                    },
+                ]
+                
+            },
+            question3: {
+                type: 'checkbox',
+                text: 'select some of this options',
+                options: [
+                    {
+                        text: "option 1",
+                        label: 'label option1'
+                    },
+                    {
+                        text: "option 2",
+                        label: 'label option2'
+                    },
+                    {
+                        text: "option 3",
+                        label: 'label option3'
+                    },
+                ]
+                
+            },
+            question4: {
+                type: 'checkbox',
+                text: 'which fruit do you prefer?',
+                images: true,
+                options: [
+                    {
+                        text: "banana",
+                        label: 'banana',
+                        class: 'image-checkbox',
+                        image: 'assets/fruits/banana.jpg',
+                    },
+                    {
+                        text: "blueberries",
+                        label: 'blueberries',
+                        class: 'image-checkbox',
+                        image: './assets/fruits/blueberry.jpg',
+                    },
+                    {
+                        text: "watermelon",
+                        label: 'watermelon',
+                        class: 'image-checkbox',
+                        image: './assets/fruits/watermelon.jpg',
+                    },
+                ]
+                
+            },
+        }
+    }
+}
