8e0e1d83237a3e3387345389736c9ac062257990
carlos padial <surreal@asturnazari.com>
2021-08-07 11:16:32 +0200

    change intro logo resolution

--- public/client.js
+++ public/client.js
@@ -342,7 +342,7 @@ function preload() {
     menuBg = loadImage(ASSETS_FOLDER + MENU_BG_FILE);
     arrowButton = loadImage(ASSETS_FOLDER + "arrowButton.png");
 
-    var logoSheet = loadSpriteSheet(ASSETS_FOLDER + LOGO_FILE, 66, 82, 4);
+    var logoSheet = loadSpriteSheet(ASSETS_FOLDER + LOGO_FILE, 256, 200, 4);
     logo = loadAnimation(logoSheet);
     logo.frameDelay = 10;
 
